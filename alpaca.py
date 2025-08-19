f1022f445d7e30ca1d753aee29814a541070023f
Samuel Diop <snfdiop@outlook.com>
2025-08-19 20:23:00 +0200

    debug(ray): add process name and cmdline logging to ray head detection

--- alpaca.py
+++ alpaca.py
@@ -562,6 +562,8 @@ def cmd_ray_head(args):
     for proc in psutil.process_iter(['pid', 'name', 'cmdline', 'create_time']):
         try:
             proc_name = proc.info['name'] or ''
+            print(proc_name)
+            print(proc.info['cmdline'])
             if 'raylet' in proc_name.lower():
                 head_pid = str(proc.info['pid'])
                 break
